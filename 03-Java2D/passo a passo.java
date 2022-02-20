1cbee6d582b12982746f6dfbccd878493342d387
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-02-19 21:39:44 -0300

    Update passo a passo.java

--- 03-Java2D/passo a passo.java
+++ 03-Java2D/passo a passo.java
@@ -1,19 +1,19 @@
-public class RectApp {
-    public static void main (String[] args) {
-        Rect r1 = new Rect(1,1, 10,10);
-        r1.print();
+public class RectApp {/*definição classe RecApp*/
+    public static void main (String[] args) {/*função principal*/
+        Rect r1 = new Rect(1,1, 10,10);/*Criação de um objeto r1 com parâmetros 1,1,10,10 - o programa vai direto para a linha 10 que contem os campos do objeto Rect*/
+        r1.print();/*metodo print*/
     }
 }
 class Rect {
     int x, y;
     int w, h;
-    Rect (int x, int y, int w, int h) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+    Rect (int x, int y, int w, int h) {/*Define  as instâncias do objeto Rect*/
+        this.x = x;/*atribui o valor 1 a x*/
+        this.y = y;/*atribui o valor 1 a y*/
+        this.w = w;/*atribui o valor 10 a w*/
+        this.h = h;/*atribui o valor 10 a w* após essa linha o programa volta para a linha 3 e em seguida vai para a linha 4*/
     }
-    void print () {
+    void print () {/*é invocado o método print e esse método imprime na tela os valores atribuidos a r1*/
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
     }
