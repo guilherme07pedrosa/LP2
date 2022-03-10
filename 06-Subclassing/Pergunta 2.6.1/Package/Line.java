04b566be5d4fe9364b66c43fb62bfc09bc5a1da6
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-03-10 10:44:15 -0300

    Add files via upload

---
+++ 06-Subclassing/Pergunta 2.6.1/Package/Line.java
@@ -0,0 +1,30 @@
+/**
+ * Figuras - Linhas
+ * Guilherme Pedrosa
+ * 10/03/2022
+ */
+package figures;
+
+import java.awt.*;
+
+public class Line extends Figure {
+    int x, y;
+    int w, h;
+
+    public Line (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void print () {
+        System.out.format("Linha de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawLine(this.x,this.y, this.w,this.h);
+    }
+}
