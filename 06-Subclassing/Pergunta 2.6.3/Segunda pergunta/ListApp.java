f005d2c0ecc1888aacde910ad43962d81f669a26
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-03-13 01:08:20 -0300

    Add files via upload

---
+++ 06-Subclassing/Pergunta 2.6.3/Segunda pergunta/ListApp.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import figures.*;
+
+class ListApp {
+    public static void main (String[] args) {
+        ListFrame frame = new ListFrame();
+        frame.setVisible(true);
+    }
+}
+
+class ListFrame extends JFrame {
+    ArrayList<Figure> figs = new ArrayList<Figure>();
+    ArrayList<Triangulo> tria = new ArrayList<Triangulo>();
+    Random rand = new Random();
+
+    ListFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+
+        this.addKeyListener (
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+                    int x = rand.nextInt(350);
+                    int y = rand.nextInt(350);
+                    int w = rand.nextInt(50);
+                    int h = rand.nextInt(50);
+                    int a[] = {rand.nextInt(350),rand.nextInt(350),rand.nextInt(350)};
+                    int b[] = {rand.nextInt(350),rand.nextInt(350),rand.nextInt(350)};
+                    int c=3;
+                    
+                    if (evt.getKeyChar() == 'r') {
+                        Rect r = new Rect(x,y, w,h);
+                        figs.add(r);
+                    } 
+                    else if (evt.getKeyChar() == 'e') {
+                        figs.add(new Ellipse(x,y, w,h));
+                    }
+                    else if (evt.getKeyChar() == 'l') {
+                        figs.add(new Line(x,y, w,h));
+                    }
+                    else if (evt.getKeyChar() == 't') {
+                        tria.add(new Triangulo(a,b,c));
+                    }
+                    repaint();
+                }
+            }
+            );
+
+        this.setTitle("Lista de Figuras");
+        this.setSize(350, 350);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        for (Figure fig: this.figs) {
+            fig.paint(g);
+            }
+        for (Triangulo tri: this.tria) {
+            tri.paint(g);
+            }
+        
+    }
+}
