3e1a1b92c2832b783428bd432178f1b0b02926b5
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-03-17 16:26:06 -0300

    Add files via upload

--- Virthual Method Table/vtable_guilherme_2.c
+++ Virthual Method Table/vtable_guilherme_2.c
@@ -14,7 +14,7 @@ typedef float  (* Figure_Regular)  (struct Figure*);
 typedef struct {
     void (* print) (struct Figure*);
     float  (* area)  (struct Figure*);
-    float  (* regular)  (struct Figure*);
+    int  (* regular)  (struct Figure*);
 } Figure_vtable;
 
 typedef struct Figure {
@@ -32,12 +32,13 @@ typedef struct {
 
 void rect_print (Rect* this) {
     Figure* sup = (Figure*) this;
-    printf("Retangulo de tamanho (%.2f,%.2f) na posicao (%.2f,%.2f) e area % .\n",
+    printf("Retangulo de tamanho (%.2f,%.2f) na posicao (%.2f,%.2f) e area %.2f logico Regular %d .\n",
            this->w, this->h, sup->x, sup->y, sup->vtable->area(sup),sup->vtable->regular(sup));
            if (sup->vtable->regular(sup)==1)
             printf("Figura regular\n");
             else
             printf("Figura nao regular\n");
+
 
 }
 
@@ -49,7 +50,10 @@ float rect_area (Rect* this) {
 int rect_regular (Rect* this) {
     Figure* sup = (Figure*) this;
     if ((this->w/this->h)==1)
-    return 1;
+        return 1;
+    else
+        return 0;
+
 }
 
 
@@ -78,7 +82,7 @@ typedef struct {
 
 void tria_print (Tria* this) {
     Figure* sup = (Figure*) this;
-    printf("Triangulo de tamanho (%0.2f,%0.2f) na posicao (%0.2f,%0.2f) e area %0.2f.\n",
+    printf("Triangulo de tamanho (%0.2f,%0.2f) na posicao (%0.2f,%0.2f) e area %0.2f logico regular %d. \n",
            this->w, this->h, sup->x, sup->y, sup->vtable->area(sup), sup->vtable->regular(sup));
            if (sup->vtable->regular(sup)==1)
             printf("Figura regular\n");
@@ -86,15 +90,17 @@ void tria_print (Tria* this) {
             printf("Figura nao regular\n");
 }
 
-int tria_area (Tria* this) {
+float tria_area (Tria* this) {
     Figure* sup = (Figure*) this;
     return((this->w * this->h)/2);
 }
 
-float tria_regular (Rect* this) {
+int tria_regular (Rect* this) {
     Figure* sup = (Figure*) this;
-    if ((this->w/this->h) == 1,73/2)
-    return 1;
+    if (((this->w/this->h)<=(sqrt(3.01)/2))&&((this->w/this->h)>=(sqrt(2.99)/2)))
+        return 1;
+    else
+        return 0;
 }
 
 Figure_vtable tria_vtable = {
@@ -118,9 +124,9 @@ Tria* tria_new (float x, float y, float w, float h) {
 void main (void) {
     Figure* figs[4] = {
         (Figure*) rect_new(10,9,100,10),
-        (Figure*) tria_new(40,10,140,300),
         (Figure*) rect_new(10,10,100,100),
-        (Figure*) tria_new(210,110,305,305*1.73/2)
+        (Figure*) tria_new(40,10,140,300),
+        (Figure*) tria_new(210,110,sqrt(3)/2,1),
     };
 
     ///
