8fd01a030d4ec81434e6cb8c006728decaecf0a2
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-03-16 23:46:32 -0300

    Add files via upload

--- Virthual Method Table/vtable_guilherme_2.c
+++ Virthual Method Table/vtable_guilherme_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
     float r,g,b;
@@ -75,26 +76,37 @@ typedef struct {
     float w, h;
 } Tria;
 
-void triangulo_print (Tria* this) {
+void tria_print (Tria* this) {
     Figure* sup = (Figure*) this;
     printf("Triangulo de tamanho (%0.2f,%0.2f) na posicao (%0.2f,%0.2f) e area %0.2f.\n",
-           this->w, this->h, sup->x, sup->y, sup->vtable->area(sup));
+           this->w, this->h, sup->x, sup->y, sup->vtable->area(sup), sup->vtable->regular(sup));
+           if (sup->vtable->regular(sup)==1)
+            printf("Figura regular\n");
+            else
+            printf("Figura nao regular\n");
 }
 
-float triangulo_area (Tria* this) {
+int tria_area (Tria* this) {
     Figure* sup = (Figure*) this;
     return((this->w * this->h)/2);
 }
 
-Figure_vtable triangulo_vtable = {
-    (Figure_Print) triangulo_print,
-    (Figure_Area)  triangulo_area
+float tria_regular (Rect* this) {
+    Figure* sup = (Figure*) this;
+    if ((this->w/this->h) == 1,73/2)
+    return 1;
+}
+
+Figure_vtable tria_vtable = {
+    (Figure_Print) tria_print,
+    (Figure_Area)  tria_area,
+    (Figure_Regular) tria_regular,
 };
 
-Tria* triangulo_new (int x, int y, int w, int h) {
+Tria* tria_new (float x, float y, float w, float h) {
     Tria* this = malloc(sizeof(Tria));
     Figure* sup = (Figure*) this;
-    sup->vtable = &triangulo_vtable;
+    sup->vtable = &tria_vtable;
     sup->x = x;
     sup->y = y;
     this->w = w;
@@ -106,9 +118,9 @@ Tria* triangulo_new (int x, int y, int w, int h) {
 void main (void) {
     Figure* figs[4] = {
         (Figure*) rect_new(10,9,100,10),
-        (Figure*) triangulo_new(40,10,140,300),
+        (Figure*) tria_new(40,10,140,300),
         (Figure*) rect_new(10,10,100,100),
-        (Figure*) triangulo_new(210,110,305,130)
+        (Figure*) tria_new(210,110,305,305*1.73/2)
     };
 
     ///
