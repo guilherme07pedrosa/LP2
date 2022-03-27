4c8e13f9e197ee31da4fe11aa8f6cef5f75374d5
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-03-26 22:25:56 -0300

    Add files via upload

---
+++ Projeto1_2/Package/Line.java
@@ -0,0 +1,23 @@
+/**
+ * Figuras - Linhas
+ * Guilherme Pedrosa
+ * 10/03/2022
+ */
+
+package figures;
+
+import java.awt.*;
+
+public class Line extends Figure {
+
+    public Line (int x, int y, int w, int h) {
+        super(x,y,w,h);
+    }
+
+    
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawLine(this.x,this.y, this.w,this.h);
+    }
+}
