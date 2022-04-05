f2a2c2eaa4a638a686e8d397ef31a23233606e9f
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-04 22:28:13 -0300

    Add files via upload

--- Projeto1_2/figures/Line.java
+++ Projeto1_2/figures/Line.java
@@ -1,4 +1,5 @@
 /**
+ /**
  * Figuras - Linhas
  * Guilherme Pedrosa
  * 10/03/2022
@@ -9,15 +10,28 @@ package figures;
 import java.awt.*;
 
 public class Line extends Figure {
-
-    public Line (int x, int y, int w, int h) {
-        super(x,y,w,h);
-    }
-
     
+    
+    public Line (int x, int y, int w, int h,Color Board, Color Background){
+        super(x, y,w,h,Board,Background);
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+            this.Board=Board;
+            this.Background=Background;
 
-    public void paint (Graphics g) {
+        }
+        
+        public void colorFig(Color Background){
+			this.Background = Background;
+		
+		}
+
+        public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawLine(this.x,this.y, this.w,this.h);
+        g2d.setColor(this.Board);
+        g2d.setColor(this.Background);
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+        }
     }
-}
