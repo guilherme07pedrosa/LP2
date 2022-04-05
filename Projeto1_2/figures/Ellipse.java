df81f75ca53eea98d0c6d4d293ddcc53098d1476
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-05 12:45:42 -0300

    Add files via upload

--- Projeto1_2/figures/Ellipse.java
+++ Projeto1_2/figures/Ellipse.java
@@ -1,19 +1,35 @@
 package figures;
-
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Ellipse2D.Double;
 
 public class Ellipse extends Figure {
-
-    public Ellipse (int x, int y, int w, int h) {
-        super(x,y,w,h);
-    }
-
     
+      
+    public Ellipse (int x, int y, int w, int h,Color Board, Color Background){
+        super(x, y,w,h,Board,Background);
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+            this.Board=Board;
+            this.Background=Background;
 
-    public void paint (Graphics g) {
+        }
+        
+        public void colorFig(Color Background){
+            this.Background = Background;
+        
+        }
+
+        public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(this.Board);
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setColor(this.Background);
         g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        }
     }
-}
