d8c3060410014bd3fa6f2b32daa244b4ab206c10
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-04 22:27:47 -0300

    Add files via upload

--- Projeto1_2/figures/Figure.java
+++ Projeto1_2/figures/Figure.java
@@ -1,17 +1,21 @@
 package figures;
-
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 
 public abstract class Figure {
     public int x, y;
     public int w, h;
+        public Color Board, Background ;
   
-    public Figure(int x, int y, int w, int h){
+    public Figure(int x, int y, int w, int h,Color Board, Color Background){
         this.x=x;
         this.y=y;
         this.w=w;
         this.h=h;
-        
+            this.Board=Board;
+            this.Background=Background;
         
     }
     public void drag (int dx, int dy) {
@@ -19,9 +23,24 @@ public abstract class Figure {
         this.y += dy;
         
     }
+    
+    
+    
 
     public boolean clicked (int x, int y) {
         return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
-public abstract void paint (Graphics g);
-}
+    
+    
+    public  void resize (int rw, int rh){
+        this.w += rw;
+        this.h += rh;
+        
+    }
+    
+    
+        
+    
+    
+    public abstract void paint (Graphics g);
+    }
