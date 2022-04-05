bfdd556a353b9a47675eca91531bea7ed3b55586
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-04 22:29:29 -0300

    Add files via upload

--- Projeto1_2/figures/Rect.java
+++ Projeto1_2/figures/Rect.java
@@ -1,16 +1,33 @@
+
+
 package figures;
 import java.awt.*;
 
 public class Rect extends Figure {
 
-    public Rect (int x, int y, int w, int h) {
-        super(x,y,w,h);
-    }
+    public Rect (int x, int y, int w, int h,Color Board, Color Background){
+        super(x, y,w,h,Board,Background);
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+            this.Board=Board;
+            this.Background=Background;
+    }   
+        
 
     
 
-    public void paint (Graphics g) {
+    public void colorFig(Color Background){
+            this.Background = Background;
+        
+        }
+
+        public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(this.Board);
         g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(this.Background);
+        g2d.fillRect(this.x,this.y, this.w,this.h);
+        }
     }
-}
