d890f0203d112db6c8996e6f24a443ca8fd7c038
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-10 12:26:02 -0300

    Add files via upload

--- Projeto1_2/figures/Rect.java
+++ Projeto1_2/figures/Rect.java
@@ -15,19 +15,20 @@ public class Rect extends Figure {
             this.Background=Background;
     }   
         
-
-    
-
     public void colorFig(Color Background){
             this.Background = Background;
         
         }
+    
 
+    
         public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(this.Board);
         g2d.drawRect(this.x,this.y, this.w,this.h);
         g2d.setColor(this.Background);
         g2d.fillRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(new Color(255,0,0));
+        //g2d.fillOval(this.x,this.y, 10,10);
         }
     }
