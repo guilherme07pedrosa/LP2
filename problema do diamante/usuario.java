6836794fa45bb101f5a297db9656b1840e17de18
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-13 20:22:44 -0300

    Add files via upload

---
+++ problema do diamante/usuario.java
@@ -0,0 +1,16 @@
+
+/**
+ * Classe abstrata usuario - escreva uma descrição da classe aqui
+superclasse usuário
+ */
+public abstract class usuario{
+
+    
+    class Test extends Aluno, Professor
+{
+   public static void main(String args[])
+   {
+       Test t = new Test();
+       t.fun();
+   }
+}}
