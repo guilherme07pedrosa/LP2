ba1c52b4822d3a69d654391ffdc7af3f851950d7
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-16 10:09:11 -0300

    Add files via upload

---
+++ Iatacante.java
@@ -0,0 +1,13 @@
+
+/**
+ * professor
+ */
+
+public interface Iatacante
+{
+    
+    default void show()
+    {
+        System.out.println("chuta todas as bolas e numca pega a bola com as mãos");
+    }
+}
