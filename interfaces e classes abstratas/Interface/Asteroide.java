bb12334c609439ada8483a1346e21046d8631421
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-24 16:11:59 -0300

    Add files via upload

---
+++ interfaces e classes abstratas/Interface/Asteroide.java
@@ -0,0 +1,34 @@
+
+/**
+ * Escreva uma descrição da classe Missil aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Asteroide implements SpaceInvaders
+{
+    public int x,dx;
+    public int y,dy;
+    
+    public int getPosicaoX(){
+        x=0;
+        return x;
+    }
+    
+    public int setPosicçãoX(){
+        x=0;
+        dx=1;
+        return x+dx;
+    }
+    
+    public int getPosicaoY(){
+        y=0;
+        return y;
+    }
+    public int setPosicçãoY(){
+        y=0;
+        dy=1;
+        return y+dy;
+    }
+    
+}
