1fd848a4d937a05f5a344a4050b4961b89ea1da6
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-24 16:12:28 -0300

    Add files via upload

---
+++ interfaces e classes abstratas/Interface/NaveEspacial.java
@@ -0,0 +1,28 @@
+
+/**
+ * Escreva uma descrição da classe NaveEspacial aqui.
+ 
+ */
+public class NaveEspacial implements SpaceInvaders
+
+{
+    public int x,dx;
+    public int getPosicaoX(){
+        x=0;
+        return x;
+    }
+    
+    public int setPosicçãoX(){
+        x=0;
+        dx=1;
+        return x+dx;
+    }
+    
+    
+    public boolean Atirar(){
+        return true;
+    }
+    
+}
+    
+
