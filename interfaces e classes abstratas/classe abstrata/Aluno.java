a546b848f4151bf512bde9b3a3a1b3ddb8d76f98
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-24 13:24:40 -0300

    Add files via upload

---
+++ interfaces e classes abstratas/classe abstrata/Aluno.java
@@ -0,0 +1,14 @@
+/**
+ Aluno
+ */
+public class Aluno  extends UsuarioDaBiblioteca
+{
+   
+    public Aluno (String theNome, int numero)
+    {
+        super(theNome, numero);
+      
+    }
+
+   
+}
