a630d03a0bcfbbe97fdd535d1abf27b96c4efedb
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-04-24 13:25:34 -0300

    Add files via upload

---
+++ interfaces e classes abstratas/classe abstrata/Professor.java
@@ -0,0 +1,14 @@
+/**
+ professor
+ */
+public class Professor  extends UsuarioDaBiblioteca
+{
+   
+    public Professor(String theNome, int numero)
+    {
+        super(theNome, numero);
+      
+    }
+
+   
+}
