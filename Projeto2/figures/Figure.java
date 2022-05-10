2fde32a860faa59018a53cd4dee304cd7db47477
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-10 19:49:55 -0300

    Add files via upload

---
+++ Projeto2/figures/Figure.java
@@ -0,0 +1,51 @@
+package figures;
+import java.io.Serializable;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import ivisible.IVisible;
+
+    public abstract class Figure implements IVisible, Serializable  {
+        public int x, y;
+        public int w, h;
+        public Color Board;
+        public Color Background;
+       
+  
+    public Figure(int x, int y, int w, int h,Color Board, Color Background){
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+            this.Board=Board;
+            this.Background=Background;
+        
+    }
+    
+    
+  
+ 
+public void drag (int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+        
+    }
+    
+    public boolean clicked (int x, int y) {
+        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
+    }
+    
+    
+    public  void resize (int rw, int rh){
+        this.w += rw;
+        this.h += rh;
+        
+    }
+    
+    
+        
+    
+    
+    public abstract void paint (Graphics g);
+    }
