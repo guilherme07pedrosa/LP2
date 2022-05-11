b44cae7e9c76c569e37f06d4f99c7c50b8ef0ae2
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-11 09:52:09 -0300

    Add files via upload

--- Projeto2/figures/Rect.java
+++ Projeto2/figures/Rect.java
@@ -22,14 +22,20 @@ public class Rect extends Figure {
     
 
     
-        public void paint (Graphics g/*,boolean focused*/) {
+       public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-      
-        g2d.setColor(this.Board);
-        g2d.drawRect(this.x,this.y, this.w,this.h);
-        g2d.setColor(this.Background);
-        g2d.fillRect(this.x,this.y, this.w,this.h);
-        g2d.setColor(new Color(255,0,0));
-        
+
+        g2d.setColor(Color.white);
+        g2d.fillRect(this.x, this.y, this.w, this.h);
+
+        if (focused){
+            g2d.setColor(Color.red);
+            g2d.drawRect(this.x, this.y, this.w,this.h);
         }
+        else {
+            g2d.setColor(Color.black);
+            g2d.drawRect(this.x, this.y, this.w, this.h);
+        }
+
+    }
     }
