1e47f19bbb8ee2b2c1017a77515b4a04166a4982
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-11 15:34:19 -0300

    Add files via upload

--- Projeto2/figures/Ellipse.java
+++ Projeto2/figures/Ellipse.java
@@ -7,9 +7,9 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 
 public class Ellipse extends Figure {
-     
     
-   public Ellipse (int x, int y, int w, int h,Color Board, Color Background){
+      
+    public Ellipse (int x, int y, int w, int h,Color Board, Color Background){
         super(x, y,w,h,Board,Background);
         this.x=x;
         this.y=y;
@@ -17,7 +17,6 @@ public class Ellipse extends Figure {
         this.h=h;
             this.Board=Board;
             this.Background=Background;
-           
 
         }
         
@@ -26,15 +25,21 @@ public class Ellipse extends Figure {
         
         }
 
-        public  void paint (Graphics g/*,boolean focused*/) {
+        public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-        
-        
-        g2d.setColor(this.Board);
-        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-        g2d.setColor(this.Background);
-        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-        g2d.setColor(new Color(255,0,0));
-        //g2d.fillOval(this.x,this.y, 10,10);
+
+        g2d.setColor(Color.white);
+        g2d.fillOval(this.x, this.y, this.w, this.h);
+
+        if (focused){
+            g2d.setColor(Color.red);
+            g2d.drawOval(this.x, this.y, this.w,this.h);
         }
+        else {
+            g2d.setColor(Color.black);
+            g2d.drawOval(this.x, this.y, this.w, this.h);
+        }
+
     }
+    }
+    
