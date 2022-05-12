2a2971cc4644c5ef473a16630746f8050cfc5f42
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-12 18:53:24 -0300

    Add files via upload

--- Projeto1_2/ivisible/IVisible.java
+++ Projeto1_2/ivisible/IVisible.java
@@ -1,11 +1,8 @@
 package ivisible;
-/**
- Guilherme Pedrosa
- */
 
 import java.awt.Graphics;
 
 public interface IVisible {
     public boolean clicked (int x, int y);
-    public void    paint   (Graphics g);
+    public void    paint   (Graphics g, boolean focused);
 }
