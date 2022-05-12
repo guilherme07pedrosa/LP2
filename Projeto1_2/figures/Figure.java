9b07d59fa9a144c03433e693e998c94543706fc8
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-12 18:55:21 -0300

    Add files via upload

--- Projeto1_2/figures/Figure.java
+++ Projeto1_2/figures/Figure.java
@@ -1,22 +1,18 @@
+
 package figures;
+import java.io.Serializable;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import ivisible.IVisible;
 
-    public abstract class Figure implements IVisible {
-        protected int x, y;
-        protected int w, h;
-        public Color Board;
-        public void setBoard(Color Board){this.Board=Board;}
-        public Color getBoard(){return Board;};
-        public Color Background;
-        public void setBackground(Color Background){this.Background=Background;}
-        public Color getBackground(){return Background;};
-       
+public abstract class Figure implements IVisible, Serializable {
+    public int x, y;
+    public int w, h;
+        public Color Board, Background ;
   
-    protected Figure(int x, int y, int w, int h,Color Board, Color Background){
+    public Figure(int x, int y, int w, int h,Color Board, Color Background){
         this.x=x;
         this.y=y;
         this.w=w;
@@ -25,12 +21,15 @@ import ivisible.IVisible;
             this.Background=Background;
         
     }
-   public void drag (int dx, int dy) {
+    public void drag (int dx, int dy) {
         this.x += dx;
         this.y += dy;
         
     }
     
+    
+    
+
     public boolean clicked (int x, int y) {
         return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
@@ -46,5 +45,5 @@ import ivisible.IVisible;
         
     
     
-    public abstract void paint (Graphics g);
+    public abstract void paint (Graphics g, boolean focused);
     }
