50ca08dca85a67ccd9e7bba60424dbc0606aa214
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-19 18:23:22 -0300

    Add files via upload

--- Projeto1_2/figures/Ellipse.java
+++ Projeto1_2/figures/Ellipse.java
@@ -11,19 +11,19 @@ public class Ellipse extends Figure {
       
     public Ellipse (int x, int y, int w, int h,Color Board, Color Background){
         super(x, y,w,h,Board,Background);
-        this.x=x;
-        this.y=y;
-        this.w=w;
-        this.h=h;
+      //  this.x=x;
+       // this.y=y;
+       // this.w=w;
+      //  this.h=h;
             this.Board=Board;
             this.Background=Background;
 
         }
         
-        public void colorFig(Color Background){
-            this.Background = Background;
+       // public void colorFig(Color Background){
+         //   this.Background = Background;
         
-        }
+       // }
 
         public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
