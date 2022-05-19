4e03001ef5694004fef6bc9cd8596e9c79ad48ba
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-19 18:30:59 -0300

    Add files via upload

--- Projeto1_2/figures/Triangulo.java
+++ Projeto1_2/figures/Triangulo.java
@@ -1,8 +1,4 @@
-/**
- * Figuras - Triangulo
- * Guilherme Pedrosa
- * 10/03/2022
- */
+
 package figures;
 
 import java.awt.*;
@@ -14,10 +10,10 @@ public class Triangulo extends Figure {
 
     public Triangulo (int x, int y, int w, int h,Color Board, Color Background){
         super(x, y,w,h,Board,Background);
-        this.x=x;
-        this.y=y;
-        this.w=w;
-        this.h=h;
+        //this.x=x;
+      //  this.y=y;
+        //this.w=w;
+       // this.h=h;
             this.Board=Board;
             this.Background=Background;
             a= new int[] {x,x+w/2,x-w/2};
@@ -46,13 +42,29 @@ public class Triangulo extends Figure {
         
         }
 
-        public void paint (Graphics g) {
+        
+       
+         public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.Board);
-        g2d.drawPolygon(this.a,this.b, 3);
-        g2d.setColor(this.Background);
+
+        g2d.setColor(Color.white);
         g2d.fillPolygon(this.a,this.b, 3);
-        g2d.setColor(new Color(255,0,0));
-       // g2d.fillOval(this.x,this.y, 10,10);
+
+        if (focused){
+            g2d.setColor(Color.red);
+           g2d.drawPolygon(this.a,this.b, 3);
         }
+        else {
+            g2d.setColor(Color.black);
+           g2d.drawPolygon(this.a,this.b, 3);
+           
+        }
+
     }
+    }
+         
+        
+        
+        
+        
+    
