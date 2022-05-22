74c20d4fdc3d01b3b652a1084e3e088c41e787ad
guilherme07pedrosa <92689483+guilherme07pedrosa@users.noreply.github.com>
2022-05-22 13:49:20 -0300

    Add files via upload

--- Projeto1_2/figures/Circulo.java
+++ Projeto1_2/figures/Circulo.java
@@ -4,37 +4,28 @@ package figures;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
+import java.util.*;
 
 public class Circulo extends Figure {
-
+   
+    
     public Circulo (int x, int y, int w, int h,Color Board, Color Background){
         super(x, y,w,h,Board,Background);
-      //  this.x=x;
-      //  this.y=y;
-      //  this.w=w;
-      //  this.h=h;
-            this.Board=Board;
-            this.Background=Background;
-
+      
         }
         
-        public void colorFig(Color Background){
-            this.Background = Background;
         
-        }
 
        public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
 
-        g2d.setColor(Color.gray);
-        g2d.fillOval(this.x, this.y, this.w, this.w);
-
+       
         if (focused){
             g2d.setColor(Color.red);
             g2d.drawOval(this.x, this.y, this.w,this.w);
         }
         else {
-            g2d.setColor(Color.gray);
+            g2d.setColor(this.Board);
             g2d.fillOval(this.x, this.y, this.w, this.w);
         }
     }
